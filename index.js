1c7dda10abde7e0c819c65b97db73820029ecdb1
disavey <dsavied@gmail.com>
2024-05-06 14:38:16 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
     const app = express();
 
-    app.get("/", (req, res) => res.send("Express Saviero"));
+    app.get("/", (req, res) => res.send("Express Saviero Dimas Prasetyo"));
 
     app.listen(3000, () => console.log("Server ready on port 3000."));
 
